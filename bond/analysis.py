bcac72c537e6ab7f6e4a93a3d2ec10e061031655
wangfy8599 <wangfy8599@sina.com>
2022-10-23 23:37:42 +0800

    1

--- bond/analysis.py
+++ bond/analysis.py
@@ -1,8 +1,17 @@
 import pandas as pd
 
 
+def format_data(df):
+    df = df.rename(columns=lambda s: s.replace(" ", ""))
+    return df
+
+
+def main():
+    with open(r"D:\github\dataextractor\bond\2022\10\23.html", "r") as f:
+        df_tables = pd.read_html(f)
+        df = format_data(df_tables[0])
+        print(df)
+
 
 if __name__ == "__main__":
-    with open(r"D:\github\dataextractor\bond\2022\10\23.html", "r") as f:
-        df_table = pd.read_html(f)
-        print(df_table)
+    main()
